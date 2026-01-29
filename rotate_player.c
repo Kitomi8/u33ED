8d38c16c81701aabe7901c7fc63d2adce93d2c61
rtoky-fa <tokysariakartf@gmail.com>
2026-01-29 15:02:39 +0300

    add rotate

--- rotate_player.c
+++ rotate_player.c
@@ -6,7 +6,7 @@
 /*   By: rtoky-fa <rtoky-fa@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 11:46:06 by rtoky-fa          #+#    #+#             */
-/*   Updated: 2026/01/29 15:01:12 by rtoky-fa         ###   ########.fr       */
+/*   Updated: 2026/01/29 15:02:30 by rtoky-fa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void    rotate_player(t_ray *ray, double angle)
 
     old_dir_x = ray->dirX;
     ray->dirX = ray->dirX * cos(angle) - ray->dirY * sin(angle);
-    ray->dirY = old_dir_x * sin(angle) + ray->dirY * cos(angle);~
+    ray->dirY = old_dir_x * sin(angle) + ray->dirY * cos(angle);
     old_plane_x = ray->planeX;
     ray->planeX = ray->planeX * cos(angle) - ray->planeY * sin(angle);
     ray->planeY = old_plane_x * sin(angle) + ray->planeY * cos(angle);
